a775eb14cc71e8560e2f1d3d27fb4bc2a0a5340a
MTaylor124 <matthew_taylor1@student.uml.edu>
2019-07-25 08:56:01 -0400

    Updated for deploy

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://mtaylor-notepad.herokuapp.com',
-  development: 'http://localhost:7165'
+  development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
